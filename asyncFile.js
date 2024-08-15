31acbd6ed9a711b828be7b0685e70236be1d6438
Nabin Poudel <nabinpoudel788@gmail.com>
2024-08-15 12:58:31 +0545

    asynchronous file system in nodejs

---
+++ asyncFile.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+//  it will write the text on define file name or create the define file name having that text if filename not present.
+fs.writeFile("asyncFile.txt", "Hello asynchronous file system.", (err, data) => {
+    console.log("file created sucessfully");
+});
+
+//  it will add another text on presented text. 
+fs.appendFile("asyncFile.txt", " My name is Nabin Poudel.", (err ,data) => {
+    console.log("another text added sucessfully");
+});
+
+/*  it will read the data from define file and utf-8 encoded original written data from that file.
+because without utf it presented buffer data */
+fs.readFile("asyncFile.xt", "utf-8", (err, data) => {
+    console.log(`Text present in given file are: ${data}`);
+})
+
+
