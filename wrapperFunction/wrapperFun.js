30d35b7aa54f12221cf50b7d397f1115f8660500
Nabin Poudel <nabinpoudel788@gmail.com>
2024-08-16 11:39:13 +0545

    module wraaper or IIFEs

---
+++ wrapperFunction/wrapperFun.js
@@ -0,0 +1,9 @@
+const names = "nabin";
+console.log(names);
+
+// !in the compilation process given code will wrape on anonymous function which is Immediately Invoke function Expression.
+// ? which make the given code is private 
+// (function () {
+//     const names = "nabin";
+//     console.log(names);
+// });
