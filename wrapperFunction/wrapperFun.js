6bcc8cbbaaca1dac0a61eb4b679caf808e93e3b5
Nabin Poudel <nabinpoudel788@gmail.com>
2024-08-18 10:25:08 +0545

    how module wrapper function work

--- wrapperFunction/wrapperFun.js
+++ wrapperFunction/wrapperFun.js
@@ -2,8 +2,10 @@ const names = "nabin";
 console.log(names);
 
 // !in the compilation process given code will wrape on anonymous function which is Immediately Invoke function Expression.
-// ? which make the given code is private 
-// (function () {
+// ? which make the given code is private. Every code which is written in nodejs is private because in background
+// * the code is wrapped by grouping operatora with anonumouse function which containe parameter require, module, 
+// * __filename, __ dirname, exports.
+// (function (exports, module, require, __filename, __dirname) {
 //     const names = "nabin";
 //     console.log(names);
 // });
