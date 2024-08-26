defbaec1e38bb8933f949ac87063b5db784eeb3a
Nabin Poudel <nabinpoudel788@gmail.com>
2024-08-26 10:05:05 +0545

    send html and json data as an response

--- express-framework/sendData/html-json.js
+++ express-framework/sendData/html-json.js
@@ -7,12 +7,14 @@ app.get("/", (req, res) => {
     res.send("<h1>Welcome to my home page</h1>");
 });
 
+// while sending json data we can also used res.json().
+// the differene of using res.json() is that it send null and undefine value as an json
 app.get("/json", (req, res) => {
-    res.send({
+    res.status(200).send({
         id:1,
         names: "nabin poudel"
     });
-})
+});
 
 app.listen(port, () => {
     console.log(`server is listening from port:`, port);
