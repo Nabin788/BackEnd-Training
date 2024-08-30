e940d9878ed22f356c04231565ea9f4b160b7596
Nabin Poudel <nabinpoudel788@gmail.com>
2024-08-30 10:18:24 +0545

    create dataBase and add multiple document

--- express-mongoDB/multiCollection/index.js
+++ express-mongoDB/multiCollection/index.js
@@ -44,10 +44,6 @@ const databaseHandle = async () => {
 
         const savedocument = await multiCol.insertMany([docOne, docTwe]);
 
-        app.get("/", (req, res) => {
-            res.send(savedocument);
-        });
-
     } catch (error) {
         console.error("document is not used fixed schema type", error);
     }
@@ -55,33 +51,6 @@ const databaseHandle = async () => {
 
 databaseHandle();
 
-const displayDocument = async () => {
-    // to read all document
-    const document = await multiCol.find();
-    console.log(document);
-
-    // to read specific document
-    const specificDoc = await multiCol.find({name: "Siwan Poudel"})
-    console.log(specificDoc);
-
-    // to update specific field of document
-    const updateField = await multiCol.updateOne({name: "Nabin Poudel"}, {$set: {address: "Rapti-01, Bhaluwang, Dang"}})
-    console.log(updateField);
-
-    // to update all field of document
-    const updateAllFiled = await multiCol.updateMany({age: 24}, {$set: {active: false}})
-    console.log(updateAllFiled);
-
-    // to delete document
-    const deletedocument = await multiCol.deleteOne({name: "Siwan Poudel"});
-    console.log(deletedocument);
-
-    // to delete all document
-    const deleteAlldocument = await multiCol.deleteMany({});
-    console.log(deleteAlldocument);
-    
-}
-displayDocument()
 
 app.listen(1000, () => {
     console.log("server is running");
