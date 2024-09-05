f8f3b69d8c737ba56fc8b88fce5acdacd4b7f7af
Nabin Poudel <nabinpoudel788@gmail.com>
2024-09-05 13:43:49 +0545

    full validated backend login with jwt token

--- project-auth/router/userRoutes.js
+++ project-auth/router/userRoutes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const userRegisterController = require("../controller/userRegisterController.js");
+const userLoginController = require("../controller/userLoginController.js");
 const router = express.Router();
 
 router.post("/register", userRegisterController);
+router.get("/login", userLoginController);
 
 module.exports = router;
